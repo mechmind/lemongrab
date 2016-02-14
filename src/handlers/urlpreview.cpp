7e3fec9c073f2862d17b76567a0cccef673acb34
Valeriy <jazzvoid@gmail.com>
2016-02-14 20:09:30 +0300

    configurable urlpreview some urlpreview refactoring, more tests for urlpreview

--- src/handlers/urlpreview.cpp
+++ src/handlers/urlpreview.cpp
@@ -1,7 +1,11 @@
 #include "urlpreview.h"
 
 #include <iostream>
+#include <map>
+
 #include <curl/curl.h>
+
+std::string formatHTMLchars(std::string input);
 
 // Curl support
 static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
@@ -29,55 +33,172 @@ static std::string CurlRequest(std::string url)
 	return readBuffer;
 }
 
+UrlPreview::UrlPreview(LemonBot *bot)
+	: LemonHandler(bot)
+{
+	readConfig(bot);
+}
+
 bool UrlPreview::HandleMessage(const std::string &from, const std::string &body)
 {
-	auto loc = body.find("http://");
-
-	if (loc == body.npos)
-		loc = body.find("https://");
-
-	if (loc == body.npos)
+	std::string site, url;
+	if (!findURL(body, site, url))
 		return false;
 
-	auto url = body.substr(loc, body.find_first_of(" \n;)", loc) - loc);
-	std::string site;
-	try {
-		site = url.substr(url.find("//") + 2, url.find("/", 9) - 2 - url.find("//")); // FIXME magic number
-	} catch (...) {
-		return false;
-	}
-
-	// FIXME make a whitelist
-	if (site != "www.youtube.com"
-			&& site != "youtube.com"
-			&& site != "youtu.be"
-			&& site != "store.steampowered.com")
+	if (_URLwhitelist.find(site) == _URLwhitelist.end())
 		return false;
 
 	std::string siteContent = CurlRequest(url);
-	auto titleStart = siteContent.find("<title>");
-	if (titleStart == siteContent.npos)
-		return false;
+	std::string title = "Can't get page title";
+	getTitle(siteContent, title);
 
-	titleStart += 7;
-
-	auto titleEnd = siteContent.find("</title>");
-	if (titleEnd == siteContent.npos)
-		return false;
-
-	titleEnd -= titleStart;
-	std::string title;
-	try {
-		title = siteContent.substr(titleStart, titleEnd);
-	} catch (...)
-	{
-		return false;
-	}
 	SendMessage(title);
 	return true;
 }
 
 const std::string UrlPreview::GetVersion() const
 {
-	return "UrlPreview: 0.1";
+	return "UrlPreview: 0.2";
 }
+
+void UrlPreview::readConfig(LemonBot *bot)
+{
+	auto unparsedWhitelist = bot->GetRawConfigValue("URLwhitelist");
+	size_t prevPos = 0;
+	for (size_t pos = 0; pos < unparsedWhitelist.length(); pos++)
+	{
+		if (unparsedWhitelist.at(pos) == ';')
+		{
+			auto url = unparsedWhitelist.substr(prevPos, pos - prevPos);
+			std::cout << "Whitelisted URL: " << url << std::endl;
+			_URLwhitelist.insert(url);
+			prevPos = pos + 1;
+		}
+	}
+}
+
+bool UrlPreview::findURL(const std::string &input, std::string &site, std::string &url)
+{
+	auto loc = input.find("http://");
+
+	if (loc == input.npos)
+		loc = input.find("https://");
+
+	if (loc == input.npos)
+		return false;
+
+	url = input.substr(loc, input.find_first_of(" \n;)", loc) - loc);
+
+	try {
+		site = url.substr(url.find("//") + 2, url.find("/", 9) - 2 - url.find("//")); // FIXME magic number
+	} catch (...) {
+		return false;
+	}
+
+	if (site.length() > 3 && site.substr(0, 4) == "www.")
+		site = site.substr(4);
+
+	return true;
+}
+
+bool UrlPreview::getTitle(const std::string &content, std::string &title)
+{
+	auto titleStart = content.find("<title>");
+	if (titleStart == content.npos)
+		return false;
+
+	titleStart += 7;
+
+	auto titleEnd = content.find("</title>");
+	if (titleEnd == content.npos)
+		return false;
+
+	titleEnd -= titleStart;
+
+	try {
+		title = content.substr(titleStart, titleEnd);
+		return true;
+	} catch (...) {
+		return false;
+	}
+}
+
+std::string formatHTMLchars(std::string input)
+{
+	std::map<std::string, std::string> chars
+			= {{"&quot;", "\""},
+			   {"&amp;", "&"},
+			   {"&lt;", "<"},
+			   {"&gt;", ">"}};
+
+	for (auto &specialCharPair : chars)
+	{
+		auto &coded = specialCharPair.first;
+		auto &decoded = specialCharPair.second;
+		size_t pos = input.find(coded);
+		while (pos != input.npos)
+		{
+			input.replace(pos, coded.length(), decoded);
+			pos = input.find(coded);
+		}
+	}
+
+	return input;
+}
+
+#ifdef _BUILD_TESTS
+
+#include <gtest/gtest.h>
+#include <fstream>
+
+class TestBot : public LemonBot
+{
+public:
+	void SendMessage(const std::string &text) const
+	{
+		_lastMessage = text;
+	}
+
+	const std::string GetRawConfigValue(const std::string &name) const
+	{
+		if (name == "URLwhitelist")
+			return "youtube.com;youtu.be;store.steampowered.com;";
+		else
+			return "";
+	}
+
+	mutable std::string _lastMessage;
+};
+
+TEST(URLPreview, HTMLSpecialChars)
+{
+	std::string input = "&quot;&amp;&gt;&lt;";
+
+	EXPECT_EQ("\"&><", formatHTMLchars(input));
+}
+
+TEST(URLPreview, GetTitle)
+{
+	TestBot testBot;
+	UrlPreview testUnit(&testBot);
+
+	{
+		std::ifstream youtube("test/youtube.html");
+		std::string content((std::istreambuf_iterator<char>(youtube)), std::istreambuf_iterator<char>());
+		youtube.close();
+		std::string title;
+		testUnit.getTitle(content, title);
+		EXPECT_EQ("\"Beach Cats\" (Ep. 3 & 4) - Bee and PuppyCat (Cartoon Hangover) - YouTube", formatHTMLchars(title));
+	}
+
+	{
+		std::ifstream steam("test/steam.html");
+		std::string content((std::istreambuf_iterator<char>(steam)), std::istreambuf_iterator<char>());
+		steam.close();
+		std::string title;
+		testUnit.getTitle(content, title);
+		EXPECT_EQ("Undertale on Steam", formatHTMLchars(title));
+	}
+}
+
+#endif
--- src/handlers/urlpreview.h
+++ src/handlers/urlpreview.h
@@ -2,10 +2,28 @@
 
 #include "lemonhandler.h"
 
+#include <set>
+
+#ifdef _BUILD_TESTS
+#include <gtest/gtest_prod.h>
+#endif
+
 class UrlPreview : public LemonHandler
 {
 public:
-	UrlPreview(LemonBot *bot) : LemonHandler(bot) {}
+	UrlPreview(LemonBot *bot);
 	bool HandleMessage(const std::string &from, const std::string &body);
 	const std::string GetVersion() const;
+
+private:
+	void readConfig(LemonBot *bot);
+	bool findURL(const std::string &input, std::string &site, std::string &url);
+	bool getTitle(const std::string &content, std::string &title);
+
+private:
+	std::set<std::string> _URLwhitelist;
+
+#ifdef _BUILD_TESTS
+	FRIEND_TEST(URLPreview, GetTitle);
+#endif
 };
