8fa4b0625becfd249e5c96e135ef2f120e47a483
Valeriy <jazzvoid@gmail.com>
2016-02-24 03:15:31 +0300

    pager: messages now expire after 72 hours

--- src/handlers/pager.cpp
+++ src/handlers/pager.cpp
@@ -1,8 +1,16 @@
 #include "pager.h"
 
+#include <map>
+
 bool Pager::HandleMessage(const std::string &from, const std::string &body)
 {
-	// TODO: preserve messages between restarts via leveldb
+	if (body.find("!pager_stats") == 0)
+	{
+		PrintPagerStats();
+		return true;
+	}
+
+	// TODO: preserve messages between restarts via leveldb?
 	if (body.length() < 7 || body.substr(0, 6) != "!pager")
 		return false;
 
@@ -28,7 +36,10 @@ bool Pager::HandlePresence(const std::string &from, const std::string &jid, bool
 		{
 			if (message->_recepient == from || message->_recepient == jid)
 			{
-				SendMessage(from + ": " + message->_text);
+				SendMessage(from + "! You have a message >> " + message->_text);
+				_messages.erase(message++);
+			} else if(message->_expiration < std::chrono::system_clock::now()) {
+				SendMessage("Message for " + message->_recepient + " (" + message->_text + ") has expired");
 				_messages.erase(message++);
 			} else {
 				++message;
@@ -40,10 +51,26 @@ bool Pager::HandlePresence(const std::string &from, const std::string &jid, bool
 
 const std::string Pager::GetVersion() const
 {
-	return GetName() + ": beta";
+	return GetName() + ": 0.1";
 }
 
 const std::string Pager::GetHelp() const
 {
-	return "Use !pager %jid% %message%";
+	return "Use !pager %jid% %message% or !pager %nick% %message%. Paged messages are lost on bot restart or after 72 hours. Use !pager_stats to get number of paged messages";
+}
+
+void Pager::PrintPagerStats()
+{
+	std::map<std::string, int> messageStats;
+	for (auto &message : _messages)
+		++messageStats[message._recepient];
+
+	std::string messageStatsString;
+	for (auto &recepient : messageStats)
+		messageStatsString.append(" " + recepient.first + " (" + std::to_string(recepient.second) + ")");
+
+	if (messageStatsString.empty())
+		messageStatsString = " none";
+
+	SendMessage("Paged messages:" + messageStatsString);
 }
--- src/handlers/pager.h
+++ src/handlers/pager.h
@@ -3,6 +3,7 @@
 #include "lemonhandler.h"
 
 #include <list>
+#include <chrono>
 
 class Pager : public LemonHandler
 {
@@ -14,16 +15,23 @@ public:
 	const std::string GetHelp() const;
 
 private:
+	void PrintPagerStats();
+
+private:
 	class Message
 	{
 	public:
 		Message(std::string to, std::string text)
 			: _recepient(to)
 			, _text(text)
+			, _expiration(std::chrono::system_clock::now() + std::chrono::hours(72))
 		{ }
 
 		std::string _recepient;
+		std::string _senderJID;
 		std::string _text;
+
+		std::chrono::system_clock::time_point _expiration;
 	};
 
 	std::list<Message> _messages;
