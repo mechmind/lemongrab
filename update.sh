f1737ed2ac3598324448f9bdd1ad36e87525a0f8
Valeriy <jazzvoid@gmail.com>
2016-04-24 14:49:53 +0300

    fix runscript for ubuntu 16

--- update.sh
+++ update.sh
@@ -6,4 +6,4 @@ cmake .. -DGLOOXPATH=~/gloox-1.0.15/
 make || exit 1
 
 killall lemongrab
-nohup ./lemongrab lemongrab.out lemongrab.err &
+LD_PRELOAD=~/gloox-1.0.15/src/.libs/libgloox.so nohup ./lemongrab lemongrab.out lemongrab.err &
