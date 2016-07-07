4a3b3c3b78639e36918482a8d20264e2dcc5e7d3
Valeriy <jazzvoid@gmail.com>
2016-07-07 18:06:56 +0300

    fix travis build x2

--- travis/install_cpr.sh
+++ travis/install_cpr.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 wget https://github.com/whoshuu/cpr/archive/1.2.0.tar.gz && tar -xzf 1.2.0.tar.gz
-mkdir build && cd build && cmake -DUSE_SYSTEM_CURL=ON -DBUILD_CPR_TESTS=OFF ../cpr-1.2.0/
+mkdir cpr-build && cd cpr-build && cmake -DUSE_SYSTEM_CURL=ON -DBUILD_CPR_TESTS=OFF ../cpr-1.2.0/
 make
 sudo cp -R lib/* /usr/lib/
 sudo mkdir -p /usr/include/cpr
 sudo cp -R ../cpr-1.2.0/include/* /usr/include/cpr
+cd $TRAVIS_BUILD_DIR
