4a89530818791b04cf8c05cdbb768c7f8047898a
Valeriy <jazzvoid@gmail.com>
2016-07-13 23:32:18 +0300

    add update.sh to cmake

--- update.sh
+++ update.sh
@@ -2,7 +2,8 @@
 
 git pull
 cd build
-cmake .. -DGLOOX_INCLUDE_DIR=~/gloox-1.0.15/src -DGLOOX_LIBRARY=~/gloox-1.0.15/src/.libs/libgloox.so
+cmake .. -DGLOOX_INCLUDE_DIR=~/gloox-1.0.15/src -DGLOOX_LIBRARY=~/gloox-1.0.15/src/.libs/libgloox.so \
+    -DCPR_INCLUDE_DIR=~/cpr-1.2.0/include -DCPR_LIBRARY=~/cpr-1.2.0/build/lib/libcpr.a
 make || exit 1
 
 killall lemongrab
