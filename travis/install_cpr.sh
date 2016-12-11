71b94a06dc5579c42adfe2f862e994d4decbb202
Valeriy <jazzvoid@gmail.com>
2016-12-11 18:13:46 +0300

    switch from cpr 1.2.0 to 9999 becuase it has Response::Error

--- travis/install_cpr.sh
+++ travis/install_cpr.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-wget https://github.com/whoshuu/cpr/archive/1.2.0.tar.gz && tar -xzf 1.2.0.tar.gz
-mkdir cpr-build && cd cpr-build && cmake -DUSE_SYSTEM_CURL=ON -DBUILD_CPR_TESTS=OFF ../cpr-1.2.0/
+wget https://github.com/whoshuu/cpr/archive/master.zip && unzip master.zip
+mkdir cpr-build && cd cpr-build && cmake -DUSE_SYSTEM_CURL=ON -DBUILD_CPR_TESTS=OFF ../cpr-master/
 make
 sudo cp -R lib/* /usr/lib/
 sudo mkdir -p /usr/include/cpr
-sudo cp -R ../cpr-1.2.0/include/* /usr/include/cpr
+sudo cp -R ../cpr-master/include/* /usr/include/cpr
 cd $TRAVIS_BUILD_DIR
--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@
 git pull
 cd build
 cmake .. -DGLOOX_INCLUDE_DIR=~/gloox-1.0.15/src -DGLOOX_LIBRARY=~/gloox-1.0.15/src/.libs/libgloox.so \
-    -DCPR_INCLUDE_DIR=~/cpr-1.2.0/include -DCPR_LIBRARY=~/cpr-1.2.0/build/lib/libcpr.a
+    -DCPR_INCLUDE_DIR=~/cpr-9999/include -DCPR_LIBRARY=~/cpr-9999/build/lib/libcpr.a
 make || exit 1
 
 killall lemongrab
