0f72e299f5321ce8781493871624efb49b562234
Valeriy <jazzvoid@gmail.com>
2016-12-11 18:39:01 +0300

    fix travis

--- travis/install_cpr.sh
+++ travis/install_cpr.sh
@@ -4,6 +4,5 @@ wget https://github.com/whoshuu/cpr/archive/master.zip && unzip master.zip
 mkdir cpr-build && cd cpr-build && cmake -DUSE_SYSTEM_CURL=ON -DBUILD_CPR_TESTS=OFF ../cpr-master/
 make
 sudo cp -R lib/* /usr/lib/
-sudo mkdir -p /usr/include/cpr
-sudo cp -R ../cpr-master/include/* /usr/include/cpr
+sudo cp -R ../cpr-master/include/cpr /usr/include/
 cd $TRAVIS_BUILD_DIR
