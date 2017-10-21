0197099ac96d7037f2c56c021bed6ac580248c48
Valeriy Malov <jazzvoid@gmail.com>
2017-10-21 19:48:52 +0300

    put all travis scripts in one folder

---
+++ travis/install_all.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+./travis/install_cmake.sh
+./travis/install_jsoncpp.sh
+./travis/install_cpr.sh
+./travis/install_cpptoml.sh
+./travis/install_pugixml.sh
+./travis/install_sqlite_orm.sh
