4deab0d2d6b330de166e3a99eed15508d73a90ce
Valerii Malov <jazzvoid@gmail.com>
2019-03-05 19:06:06 +0300

    split implementation fixes

--- src/handlers/discord.cpp
+++ src/handlers/discord.cpp
@@ -110,14 +110,14 @@ void Discord::rclientSafeSend(const std::string &message, const std::string &cha
 		} else {
 			const int defaultSplitSize = 200;
 			const int splitThreshold = 100;
-			int lastSplit;
-			for (size_t i = 0; i <= text.length(); i += lastSplit) {
-				lastSplit = text.find_last_of(" \t\n", i + defaultSplitSize);
-				if (lastSplit - i <= splitThreshold) {
-					lastSplit = i + defaultSplitSize;
+            int lastSplit = 0;
+            for (size_t cursor = 0; cursor < text.length(); cursor = lastSplit) {
+                lastSplit = text.find_last_of(" \t\n", cursor + defaultSplitSize);
+                if (lastSplit == text.npos || lastSplit <= cursor + splitThreshold) {
+                    lastSplit = cursor + defaultSplitSize;
 				}
 
-				auto cutText = text.substr(i, lastSplit);
+                auto cutText = text.substr(cursor, lastSplit);
 				rclient->sendTextMessage(channel.empty() ? Hexicord::Snowflake(_channelID) : Hexicord::Snowflake(channel), cutText);
 				std::this_thread::sleep_for(std::chrono::seconds(1));
 			}
